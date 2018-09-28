20ea360a1a1f240780a71c5b308257f8640cb3b9
vgum <viktor.gummesson@gmail.com>
2018-09-28 22:40:36 +0200

    Left and right click
    Left click properly fills a tile and right click clears a tile.

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,5 +1,4 @@
 
-
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
@@ -7,8 +6,10 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Stroke;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -26,6 +27,7 @@ public class Matrix extends JPanel implements MouseListener {
 	private MatrixTile[] columns;
 	private Rectangle rect;
 	public Rectangle tile;
+	private boolean firstTime = true;
 
 	public Matrix(int nRows) {
 		this.nRows = nRows;
@@ -43,38 +45,105 @@ public class Matrix extends JPanel implements MouseListener {
 		
 	}
 	
-	protected void paintComponent(Graphics g) {  
+	protected void paintComponent(Graphics g) {
+		 super.paintComponent(g);
+		 super.paintComponent(g);
 		 Graphics2D g2 = (Graphics2D) g;
+		 //super.paintComponent(g);
 		 Rectangle tile = null;
+		 //g2.setStroke(new BasicStroke(5));
 		 for(int i = 0; i < nRows; i++) {			 
 	    	rows = (MatrixTile[]) Spinner.get(i);
 	    	for (int j = 0; j < rows.length; j++) {
 	    		tile = rows[j].getTile();
 	    		tile.x = j * 40;
 	    		tile.y = i * 40;
-	    		rows[j].addMouseListener(rows[j]);
 	    		g2.setColor(rows[j].getColor());
-	    		g2.draw(tile);
-	    		//g2.fillRect(tile.x, tile.y, tile.width, tile.height);
+	    	//	if(!rows[j].isFilled()) {
+	    	//		g2.draw(tile);
+	    	//	} else {
+	    			int x = 69;
+	    			g2.fillRect(tile.x+1, tile.y+1, tile.width-1, tile.height-1);
+	    			g2.setColor(Color.BLACK);
+	    			g2.draw(tile);
+	    	//	}
 	    	}
 	     }
 	  }
+	
+	public void SwingUtilitiesisRightMouseButton(MouseEvent e) {
+		System.out.println("it workkkkkkks");
+	}
 
 	@Override
+	
 	public void mouseClicked(MouseEvent e) {
-		MatrixTile tile;
-		for(int i = 0; i < nRows; i++)  {
-			for(int j = 0; j < nRows; j++) {
-				tile = Spinner.get(i)[j];
-				if(tile.getTile().contains(e.getY(), e.getX())) {
-					System.out.println("Yey wow we found the tile " + Spinner.get(i)[j].id);
-					tile.changeColor(Color.GREEN);
-					break;
+		if(e.getButton() == MouseEvent.BUTTON1) {
+			//firstTime = false;
+			MatrixTile tile;
+			for(int i = 0; i < nRows; i++)  {
+				for(int j = 0; j < nRows; j++) {
+					tile = Spinner.get(i)[j];
+					if(tile.getTile().contains(e.getX(), e.getY())) {
+						System.out.println("Yey wow we found the tile " + Spinner.get(i)[j].id);
+						tile.changeColor(Color.GREEN);
+						//this.invalidate();
+						
+						//this.revalidate();
+						//this.repaint();
+						//this.rede
+						//this.setVisible(true);
+						////thirepaint();
+						//this.setVisible(false);
+						break;
+					}
+					//this.invalidate();
+					//>this.revalidate();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					//this.doLayout();
+				
 				}
-
+			}
+		}	
+		if(e.getButton() == MouseEvent.BUTTON3) {
+			MatrixTile tile;
+			for(int i = 0; i < nRows; i++)  {
+				for(int j = 0; j < nRows; j++) {
+					tile = Spinner.get(i)[j];
+					if(tile.getTile().contains(e.getX(), e.getY())) {
+						System.out.println("Yey wow we found the tile " + Spinner.get(i)[j].id);
+						tile.changeColor(Color.WHITE);
+						//this.invalidate();
+						
+						//this.revalidate();
+						//this.repaint();
+						//this.rede
+						//this.setVisible(true);
+						////thirepaint();
+						//this.setVisible(false);
+						break;
+					}
+					//this.invalidate();
+					//>this.revalidate();
+					this.repaint();
+					this.repaint();
+					this.repaint();
+					//this.doLayout();
+				
+				}
 			}
 		}
-				
 	}
 
 	@Override
--- src/MatrixTile.java
+++ src/MatrixTile.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -7,9 +6,10 @@ import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-public class MatrixTile extends JPanel implements MouseListener {
+public class MatrixTile {
 	
 	/**
 	 * 
@@ -17,7 +17,7 @@ public class MatrixTile extends JPanel implements MouseListener {
 	private static final long serialVersionUID = 1L;
 	private final int width = 40;
 	public final int id;
-	private final Color defaultColor = Color.BLACK;
+	public final Color defaultColor = Color.WHITE;
 	private Rectangle tile;
 	private Color color;
 	
@@ -25,14 +25,15 @@ public class MatrixTile extends JPanel implements MouseListener {
 		this.id = id;
 		tile = new Rectangle(width,width);
 		this.color = color;
-		addMouseListener(this);
+		//addMouseListener(this);
 	}
 	
 	public MatrixTile(int id) {
 		tile = new Rectangle(width,width);
 		this.id = id;
 		this.color = defaultColor;
-		addMouseListener(this);
+		//this.setBorder(BorderFactory.createLineBorder(Color.black));
+		//addMouseListener(this);
 	}
 	
 	public Color getColor() {
@@ -45,39 +46,6 @@ public class MatrixTile extends JPanel implements MouseListener {
 	public Rectangle getTile() {
 		return tile;
 	}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		if(tile.contains(e.getX(),e.getY())) {
-			System.out.println("HEEEEEEY");
-		}
-		System.out.println("You just clicked a tile hurray!!!!");
-		
-	}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		System.out.println("This is a tile");
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
